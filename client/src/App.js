93b0677eb71379a2620566e10036787c215e9f01
Kvelail <contactkka@gmail.com>
2022-01-18 19:01:13 +0100

    folder and files setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,48 @@
-import { 
-  BrowserRouter as Router,
-  Routes as Switch,
-  Route,
-  Link
-} from 'react-router-dom'
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Routes as Switch,
+    Route,
+    Link,
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <Router>
-      <nav>
-        <button>
-          <Link to="/"> Home </Link>
-        </button> 
-        <button>
-          <Link to="/play"> Play </Link>
-        </button>
-        <button>
-          <Link to="/about"> About Us </Link>
-        </button>
-        <button>
-          <Link to="/collections"> Manage Collections </Link>
-        </button>
-      </nav>
-      <Switch>
-        <Route path="/" element={<h1>Home</h1>} /> 
-        <Route path="/play" element={<h1>Play</h1>} />
-        <Route path="/about" element={<h1>About Us</h1>} />
-        <Route path="/collections" element={<h1>Manage Collections</h1>} />
-      </Switch>
-      
-    </Router>
-  );
-}
+import './styles/app.scss';
+
+import { Box } from '@mui/material';
+
+import Home from './components/home/Home';
+
+const App = () => {
+    return (
+        <Router>
+            <Box className='app'>
+                <nav>
+                    <button>
+                        <Link to='/'> Home </Link>
+                    </button>
+                    <button>
+                        <Link to='/play'> Play </Link>
+                    </button>
+                    <button>
+                        <Link to='/about'> About Us </Link>
+                    </button>
+                    <button>
+                        <Link to='/collections'> Manage Collections </Link>
+                    </button>
+                </nav>
+                <Box border={1} maxWidth='1000px' margin='0 auto'>
+                    <Switch>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/play' element={<h1>Play</h1>} />
+                        <Route path='/about' element={<h1>About Us</h1>} />
+                        <Route
+                            path='/collections'
+                            element={<h1>Manage Collections</h1>}
+                        />
+                    </Switch>
+                </Box>
+            </Box>
+        </Router>
+    );
+};
 
 export default App;
---
+++ client/src/components/home/Home.js
@@ -0,0 +1,5 @@
+const Home = () => {
+    return <h1>HOMEEE</h1>;
+};
+
+export default Home;
