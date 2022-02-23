48a12e1f53bc40286f20224d83571be7657ddfb7
Kvelail <contactkka@gmail.com>
2022-02-23 20:00:15 +0100

    shuffle flashcards added

--- client/src/utils.js
+++ client/src/utils.js
@@ -44,3 +44,14 @@ export const getStateCollectionNames = (collections) => {
 
     return existingStateCollectionNames;
 };
+
+// Fisher-Yates shuffle function
+export const shuffleData = (data) => {
+    for (let i = data.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+
+        [data[i], data[j]] = [data[j], data[i]];
+    }
+
+    return data;
+};
