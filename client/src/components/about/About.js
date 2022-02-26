fc257f6657de94996acee1921c06e66d95db80ba
Proioxis4 <lucadaniele444@gmail.com>
2022-02-26 10:44:38 +0100

    modified About component

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -7,16 +7,35 @@ import { AiFillGithub } from 'react-icons/ai';
 const About = () => {
     return (
         <Box className='about' maxWidth='600px' color='white'>
-            <Typography variant='h2'>About Us</Typography>
-            <Typography mt={5} fontSize='1.5rem'>
-                Hello everyone,
+            <Typography variant='h2'>About Kadeu</Typography>
+            <Typography mt={2} mb={4} fontSize='1.5rem'>
+            Kadeu is an interactive learning experience where you can test 
+            your knowledge in anything you can imagine. It is designed to help 
+            the user increase their understanding about a chosen topic by using  
+            <Link href="https://en.wikipedia.org/wiki/Spaced_repetition" underline="none"> Spaced Repetition</Link> which has been proven to be one of the best learning 
+            methods for enhancing long-term memory capabilities.
+            </Typography>
+            <Typography variant='h2' mt={4}>How to use Kadeu</Typography>
+            <Typography mt={2} mb={4} fontSize='1.5rem'>
+            This is a brief guide on how to use all the features of Kadeu.
+            <br /><br />
+            Get started by creating a collection inside "Manage Collections", 
+            there will be already plenty of collections available. 
+            <br />If you found the topic 
+            you want to learn more about, head to the "Play" tab and start learning.
+            <br />If you haven't,
+            create a new collection, add as much cards as you want and start testing yourself! 
+            <br />Before playing you can choose between Quiz Mode and Standard Mode by pressing a checkbox:
+            Standard Mode will let you play the <Link href="https://en.wikipedia.org/wiki/Flashcard" underline="none">Classic Game Mode</Link>,
+            Quiz Mode is an advanced version of the Standard Mode, where you can select the options in a more interactive fashion.
+            <br />
+
+            That's everything you have to know about Kadeu to get started, head to the Play section and start learning!
+            
             </Typography>
             <Typography mt={2} mb={4} fontSize='1.5rem'>
-                we are Chingu voyage #36 team. Our team name is Geckos-09 and
-                this is our project made with React on frontend and Express with
-                MongoDB on backend. Here you can check our GitHub repo and hope
-                you'll like the application !
-            </Typography>
+                You can check out our GitHub repository down below where you can find
+                any additional information about the project. </Typography>
             <Link
                 href='https://github.com/chingu-voyages/v36-geckos-team-09'
                 target='_blank'
--- client/src/static.js
+++ client/src/static.js
@@ -1,7 +1,7 @@
 export const NAVBAR_BUTTONS = [
     { id: 0, text: 'home', link: '/' },
     { id: 1, text: 'play', link: '/play' },
-    { id: 2, text: 'about us', link: '/about' },
+    { id: 2, text: 'about Kadeu', link: '/about' },
     { id: 3, text: 'manage collections', link: '/manage-collections' },
 ];
 
