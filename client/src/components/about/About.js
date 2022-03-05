0c8f126d765f2effce9c168fb7dc27b26fbfcd5f
Proioxis4 <lucadaniele444@gmail.com>
2022-03-05 17:45:23 +0100

    final commit

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -29,37 +29,33 @@ const About = () => {
                 How to use Kadeu
             </Typography>
             <Typography mt={2} mb={4} fontSize='1.4rem'>
-                This is a brief guide on how to use all the features of Kadeu.
-                <br />
-                <br />
-                Get started by creating a collection inside "Manage
-                Collections", there will be already plenty of collections
-                available.
-                <br />
-                If you found the topic you want to learn more about, head to the
-                "Play" tab and start learning.
-                <br />
-                If you haven't, create a new collection, add as much cards as
-                you want and start testing yourself!
-                <br />
-                Before playing you can choose between Quiz Mode and Standard
-                Mode by pressing a checkbox: Standard Mode will let you play the{' '}
-                <Link
+            <Typography variant='h4' mt={4} mb={4}>
+            Collections
+            </Typography>
+            
+
+            Get started by creating a collection inside "Manage Collections", 
+            there will be already plenty of collections available. If you found the topic 
+            you want to learn more about, head to the "Play" tab and start learning. 
+            If you haven't, create a new collection, add as much cards as you want and 
+            start testing yourself!
+            
+            <Typography variant='h4' mt={4} mb={4}>
+                Play
+            </Typography>
+            
+            Before playing you can choose between Quiz Mode and Standard Mode: 
+            The Standard Mode will let you play the <Link 
                     href='https://en.wikipedia.org/wiki/Flashcard'
                     target='_blank'
                     rel='noopener noreferrer'
                     underline='none'
                     color='secondary'
-                    fontWeight={700}
-                >
-                    Classic Game Mode
-                </Link>
-                , Quiz Mode is an advanced version of the Standard Mode, where
-                you can select the options in a more interactive fashion.
-                <br />
-                <br />
-                That's everything you have to know about Kadeu to get started,
-                head to the Play section and start learning!
+                    fontWeight={700}>classic flashcards game</Link>, 
+            Quiz Mode is an advanced version of the Standard Mode, where you can select the options in a more interactive fashion.
+            </Typography>
+            <Typography variant='h2' mt={4} mb={4}>
+                Github
             </Typography>
             <Typography mt={2} mb={4} fontSize='1.4rem'>
                 You can check out our GitHub repository down below where you can
