4a86b8a2995063212e38bd9cc60e1dffc2fffb73
Proioxis4 <lucadaniele444@gmail.com>
2022-03-05 19:15:56 +0100

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import flashcards from './api/flashcards.route.js';
+const path = require('path');
 
 const app = express();
 
@@ -8,6 +9,15 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/flashcards', flashcards);
+
+//server static assets
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*',(req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+    })
+}
 app.use('*', (req, res) => res.status(404).json({ error: 'not found' }));
 
 export default app;
