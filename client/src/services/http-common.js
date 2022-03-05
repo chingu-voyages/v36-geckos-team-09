f590dd022e3d6cf660f1e13f89159e975421f94d
Proioxis4 <lucadaniele444@gmail.com>
2022-03-05 21:44:15 +0100

    axios url change

--- client/src/services/http-common.js
+++ client/src/services/http-common.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:4000/api/v1/flashcards',
+    /* baseURL: 'http://localhost:4000/api/v1/flashcards', for dev */
+    baseURL: 'https://kadeu.herokuapp.com/api/v1/',  
     headers: {
         'Content-type': 'application/json',
     },
